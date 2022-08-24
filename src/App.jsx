faf59d908d1dba68d8d371ba65bb686ac6ce9548
Joel Contreras <jocontreras246@gmail.com>
2022-08-24 18:30:08 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+
+  return (
+      <div>
+        <img src="public/image20.png" className="logo" alt="menu" />
+      </div>
+  );
+}
+
+export default App;
